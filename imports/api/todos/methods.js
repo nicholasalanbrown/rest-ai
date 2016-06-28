94c5e9c09adb79063a35b9080fa8a3b5bcb07fda
Nicholas Alan Brown <nicholasalanbrown@gmail.com>
2016-06-28 16:11:40 -0400

    Adding response data to API

--- imports/api/todos/methods.js
+++ imports/api/todos/methods.js
@@ -32,6 +32,22 @@ export const insert = new ValidatedMethod({
   },
 });
 
+Meteor.methods({
+  helloWorld: function () {
+    console.log('hello world!');
+    return
+      {
+        "speech": "Barack Hussein Obama II is the 44th and current President of the United States.",
+        "displayText": "Barack Hussein Obama II is the 44th and current President of the United States, and the first African American to hold the office. Born in Honolulu, Hawaii, Obama is a graduate of Columbia University   and Harvard Law School, where ",
+        "data": {...},
+        "contextOut": [...],
+        "source": "DuckDuckGo"
+      }
+  }
+});
+
+heroku config:set ROOT_URL=https://rest-ai.herokuapp.com
+
 export const setCheckedStatus = new ValidatedMethod({
   name: 'todos.makeChecked',
   validate: new SimpleSchema({
